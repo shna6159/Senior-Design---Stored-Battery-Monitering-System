af06543def218313b7b0c66fa28fb1f0164504b2
shna6159 <shna6159@colorado.edu>
2021-10-17 10:49:25 -0600

    Add an example project and setup paths in makefile

---
+++ src/ble_app_blinky/pca10059/s140/armgcc/_build/nrf52840_xxaa/app_scheduler.c.d
@@ -0,0 +1,132 @@
+_build/nrf52840_xxaa/app_scheduler.c.o: \
+ ../../../../SDK/components/libraries/scheduler/app_scheduler.c \
+ ../../../../SDK/components/libraries/util/sdk_common.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\_default_types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\features.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_newlib_version.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_intsup.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\string.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_ansi.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\newlib.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\config.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\ieeefp.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\reent.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_ansi.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\_types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\lock.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\cdefs.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\string.h \
+ ../config/sdk_config.h \
+ ../../../../SDK/components/libraries/util/nordic_common.h \
+ ../../../../SDK/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../SDK/components/libraries/util/sdk_os.h \
+ ../../../../SDK/components/libraries/util/sdk_errors.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_error.h \
+ ../../../../SDK/components/libraries/util/app_util.h \
+ ../../../../SDK/modules/nrfx/mdk/nrf.h \
+ ../../../../SDK/modules/nrfx/mdk/nrf52840.h \
+ ../../../../SDK/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../SDK/components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../SDK/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../SDK/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../SDK/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../SDK/modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../SDK/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../SDK/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../SDK/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../SDK/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf52/nrf_mbr.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_svc.h \
+ ../../../../SDK/components/libraries/util/sdk_macros.h \
+ ../../../../SDK/components/libraries/util/nrf_assert.h \
+ ../../../../SDK/components/libraries/scheduler/app_scheduler.h \
+ ../../../../SDK/components/libraries/util/app_error.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\stdio.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdarg.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_pthreadtypes.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\stdio.h \
+ ../../../../SDK/components/libraries/util/app_error_weak.h \
+ ../../../../SDK/components/libraries/util/app_util.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\stdlib.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\stdlib.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_soc.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_svc.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_error.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_error_soc.h \
+ ../../../../SDK/components/libraries/util/nrf_assert.h \
+ ../../../../SDK/components/libraries/util/app_util_platform.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_nvic.h \
+ ../../../../SDK/components/libraries/util/app_error.h
+../../../../SDK/components/libraries/util/sdk_common.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\stdint.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\_default_types.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\features.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_newlib_version.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_intsup.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_stdint.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\string.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_ansi.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\newlib.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\config.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\ieeefp.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\reent.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_ansi.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_types.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\_types.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\lock.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\cdefs.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\string.h:
+../config/sdk_config.h:
+../../../../SDK/components/libraries/util/nordic_common.h:
+../../../../SDK/modules/nrfx/mdk/compiler_abstraction.h:
+../../../../SDK/components/libraries/util/sdk_os.h:
+../../../../SDK/components/libraries/util/sdk_errors.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_error.h:
+../../../../SDK/components/libraries/util/app_util.h:
+../../../../SDK/modules/nrfx/mdk/nrf.h:
+../../../../SDK/modules/nrfx/mdk/nrf52840.h:
+../../../../SDK/components/toolchain/cmsis/include/core_cm4.h:
+../../../../SDK/components/toolchain/cmsis/include/cmsis_version.h:
+../../../../SDK/components/toolchain/cmsis/include/cmsis_compiler.h:
+../../../../SDK/components/toolchain/cmsis/include/cmsis_gcc.h:
+../../../../SDK/components/toolchain/cmsis/include/mpu_armv7.h:
+../../../../SDK/modules/nrfx/mdk/system_nrf52840.h:
+../../../../SDK/modules/nrfx/mdk/nrf52840_bitfields.h:
+../../../../SDK/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+../../../../SDK/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+../../../../SDK/modules/nrfx/mdk/compiler_abstraction.h:
+../../../../SDK/components/softdevice/s140/headers/nrf52/nrf_mbr.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_svc.h:
+../../../../SDK/components/libraries/util/sdk_macros.h:
+../../../../SDK/components/libraries/util/nrf_assert.h:
+../../../../SDK/components/libraries/scheduler/app_scheduler.h:
+../../../../SDK/components/libraries/util/app_error.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\stdio.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdarg.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\types.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_pthreadtypes.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\types.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\stdio.h:
+../../../../SDK/components/libraries/util/app_error_weak.h:
+../../../../SDK/components/libraries/util/app_util.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\stdlib.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\stdlib.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_soc.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_svc.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_error.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_error_soc.h:
+../../../../SDK/components/libraries/util/nrf_assert.h:
+../../../../SDK/components/libraries/util/app_util_platform.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_nvic.h:
+../../../../SDK/components/libraries/util/app_error.h:
---
+++ src/ble_app_blinky/pca10059/s140/armgcc/_build/nrf52840_xxaa/nrf_sdh_soc.c.d
@@ -0,0 +1,146 @@
+_build/nrf52840_xxaa/nrf_sdh_soc.c.o: \
+ ../../../../SDK/components/softdevice/common/nrf_sdh_soc.c \
+ ../../../../SDK/components/libraries/util/sdk_common.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\_default_types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\features.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_newlib_version.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_intsup.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_stdint.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\string.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_ansi.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\newlib.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\config.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\ieeefp.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\reent.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_ansi.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\_types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\lock.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\cdefs.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\string.h \
+ ../config/sdk_config.h \
+ ../../../../SDK/components/libraries/util/nordic_common.h \
+ ../../../../SDK/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../SDK/components/libraries/util/sdk_os.h \
+ ../../../../SDK/components/libraries/util/sdk_errors.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_error.h \
+ ../../../../SDK/components/libraries/util/app_util.h \
+ ../../../../SDK/modules/nrfx/mdk/nrf.h \
+ ../../../../SDK/modules/nrfx/mdk/nrf52840.h \
+ ../../../../SDK/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../SDK/components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../SDK/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../SDK/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../SDK/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../SDK/modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../SDK/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../SDK/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../SDK/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../SDK/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf52/nrf_mbr.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_svc.h \
+ ../../../../SDK/components/libraries/util/sdk_macros.h \
+ ../../../../SDK/components/libraries/util/nrf_assert.h \
+ ../../../../SDK/components/softdevice/common/nrf_sdh_soc.h \
+ ../../../../SDK/components/libraries/experimental_section_vars/nrf_section_iter.h \
+ ../../../../SDK/components/libraries/experimental_section_vars/nrf_section.h \
+ ../../../../SDK/components/libraries/util/nordic_common.h \
+ ../../../../SDK/components/libraries/util/nrf_assert.h \
+ ../../../../SDK/components/libraries/util/app_util.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_soc.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_svc.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_error.h \
+ ../../../../SDK/components/softdevice/s140/headers/nrf_error_soc.h \
+ ../../../../SDK/components/softdevice/common/nrf_sdh.h \
+ ../../../../SDK/components/libraries/util/sdk_errors.h \
+ ../../../../SDK/components/libraries/util/app_error.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\stdio.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdarg.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_pthreadtypes.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\types.h \
+ c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\stdio.h \
+ ../../../../SDK/components/libraries/util/app_error_weak.h \
+ ../../../../SDK/components/libraries/log/nrf_log.h \
+ ../../../../SDK/components/libraries/experimental_section_vars/nrf_section.h \
+ ../../../../SDK/components/libraries/strerror/nrf_strerror.h \
+ ../../../../SDK/components/libraries/log/src/nrf_log_internal.h \
+ ../../../../SDK/components/libraries/log/nrf_log_instance.h \
+ ../../../../SDK/components/libraries/log/nrf_log_types.h \
+ ../../../../SDK/components/libraries/log/nrf_log_types.h
+../../../../SDK/components/libraries/util/sdk_common.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\stdint.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\_default_types.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\features.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_newlib_version.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_intsup.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_stdint.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\string.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_ansi.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\newlib.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\config.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\ieeefp.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\reent.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\_ansi.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_types.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\_types.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\lock.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\cdefs.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\string.h:
+../config/sdk_config.h:
+../../../../SDK/components/libraries/util/nordic_common.h:
+../../../../SDK/modules/nrfx/mdk/compiler_abstraction.h:
+../../../../SDK/components/libraries/util/sdk_os.h:
+../../../../SDK/components/libraries/util/sdk_errors.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_error.h:
+../../../../SDK/components/libraries/util/app_util.h:
+../../../../SDK/modules/nrfx/mdk/nrf.h:
+../../../../SDK/modules/nrfx/mdk/nrf52840.h:
+../../../../SDK/components/toolchain/cmsis/include/core_cm4.h:
+../../../../SDK/components/toolchain/cmsis/include/cmsis_version.h:
+../../../../SDK/components/toolchain/cmsis/include/cmsis_compiler.h:
+../../../../SDK/components/toolchain/cmsis/include/cmsis_gcc.h:
+../../../../SDK/components/toolchain/cmsis/include/mpu_armv7.h:
+../../../../SDK/modules/nrfx/mdk/system_nrf52840.h:
+../../../../SDK/modules/nrfx/mdk/nrf52840_bitfields.h:
+../../../../SDK/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+../../../../SDK/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+../../../../SDK/modules/nrfx/mdk/compiler_abstraction.h:
+../../../../SDK/components/softdevice/s140/headers/nrf52/nrf_mbr.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_svc.h:
+../../../../SDK/components/libraries/util/sdk_macros.h:
+../../../../SDK/components/libraries/util/nrf_assert.h:
+../../../../SDK/components/softdevice/common/nrf_sdh_soc.h:
+../../../../SDK/components/libraries/experimental_section_vars/nrf_section_iter.h:
+../../../../SDK/components/libraries/experimental_section_vars/nrf_section.h:
+../../../../SDK/components/libraries/util/nordic_common.h:
+../../../../SDK/components/libraries/util/nrf_assert.h:
+../../../../SDK/components/libraries/util/app_util.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_soc.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_svc.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_error.h:
+../../../../SDK/components/softdevice/s140/headers/nrf_error_soc.h:
+../../../../SDK/components/softdevice/common/nrf_sdh.h:
+../../../../SDK/components/libraries/util/sdk_errors.h:
+../../../../SDK/components/libraries/util/app_error.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\stdio.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\lib\gcc\arm-none-eabi\10.3.1\include\stdarg.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\types.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\_pthreadtypes.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\machine\types.h:
+c:\program\ files\ (x86)\gnu\ arm\ embedded\ toolchain\10\ 2021.07\arm-none-eabi\include\sys\stdio.h:
+../../../../SDK/components/libraries/util/app_error_weak.h:
+../../../../SDK/components/libraries/log/nrf_log.h:
+../../../../SDK/components/libraries/experimental_section_vars/nrf_section.h:
+../../../../SDK/components/libraries/strerror/nrf_strerror.h:
+../../../../SDK/components/libraries/log/src/nrf_log_internal.h:
+../../../../SDK/components/libraries/log/nrf_log_instance.h:
+../../../../SDK/components/libraries/log/nrf_log_types.h:
+../../../../SDK/components/libraries/log/nrf_log_types.h:
