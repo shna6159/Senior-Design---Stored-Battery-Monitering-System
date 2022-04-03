017edbcfd4667210aa330e7d70b52e50a42d64ca
nacob123 <61359018+nacob123@users.noreply.github.com>
2022-04-03 15:17:54 -0600

    Merge

--- src/code_on_site/on_site_main_old.c
+++ src/code_on_site/on_site_main_old.c
@@ -47,6 +47,24 @@
 #include <stdio.h>
 #include <string.h>
 #include "nordic_common.h"
+#include "nrf.h"
+#include "nrf_drv_usbd.h"
+#include "nrf_drv_clock.h"
+#include "nrf_gpio.h"
+#include "nrf_delay.h"
+#include "nrf_drv_power.h"
+#include "app_usbd_core.h"
+#include "app_usbd.h"
+#include "app_usbd_string_desc.h"
+#include <app_usbd_cdc_acm.h>
+#include "app_usbd_serial_num.h"
+#include "nrf_delay.h"
+#include "boards.h"
+#include "bsp.h"
+#include "bsp_cli.h"
+#include "nrf_cli.h"
+#include "nrf_cli_uart.h"
+#include "nrf_usbd.h"
 #include "nrf_sdm.h"
 #include "ble.h"
 #include "ble_hci.h"
@@ -135,6 +153,143 @@ static ble_gap_scan_params_t const m_scan_param =
     .scan_phys     = BLE_GAP_PHY_1MBPS,
 };
 
+//#define LED_USB_RESUME      (BSP_BOARD_LED_0)
+#define LED_CDC_ACM_OPEN    (BSP_BOARD_LED_0)
+#define LED_CDC_ACM_RX      (BSP_BOARD_LED_1)
+//#define LED_CDC_ACM_TX      (BSP_BOARD_LED_1)
+
+#ifndef USBD_POWER_DETECTION
+#define USBD_POWER_DETECTION true
+#endif
+
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                    app_usbd_cdc_acm_user_event_t event);
+
+#define CDC_ACM_COMM_INTERFACE  0
+#define CDC_ACM_COMM_EPIN       NRF_DRV_USBD_EPIN2
+
+#define CDC_ACM_DATA_INTERFACE  1
+#define CDC_ACM_DATA_EPIN       NRF_DRV_USBD_EPIN1
+#define CDC_ACM_DATA_EPOUT      NRF_DRV_USBD_EPOUT1
+
+APP_USBD_CDC_ACM_GLOBAL_DEF(m_app_cdc_acm,
+                            cdc_acm_user_ev_handler,
+                            CDC_ACM_COMM_INTERFACE,
+                            CDC_ACM_DATA_INTERFACE,
+                            CDC_ACM_COMM_EPIN,
+                            CDC_ACM_DATA_EPIN,
+                            CDC_ACM_DATA_EPOUT,
+                            APP_USBD_CDC_COMM_PROTOCOL_AT_V250
+);
+
+#define READ_SIZE 1
+
+static char m_rx_buffer[READ_SIZE];
+static char m_tx_buffer[NRF_DRV_USBD_EPSIZE];
+static bool m_send_flag = 1;
+int interval;
+
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                    app_usbd_cdc_acm_user_event_t event)
+{
+    switch (event)
+    {
+        case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
+        {
+            //Setup first transfer
+            ret_code_t ret = app_usbd_cdc_acm_read(&m_app_cdc_acm,
+                                                   m_rx_buffer,
+                                                   READ_SIZE);
+            UNUSED_VARIABLE(ret);
+            break;
+        }
+        case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
+            //bsp_board_led_off(LED_CDC_ACM_OPEN);
+            break;
+        case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
+            //bsp_board_led_invert(LED_CDC_ACM_TX);
+            break;
+        case APP_USBD_CDC_ACM_USER_EVT_RX_DONE:
+        {
+            // LED ensures that RX is completed
+            bsp_board_led_invert(LED_CDC_ACM_OPEN);
+
+            // Using the amounts of bytes stored in buffer to determine interval
+            if (app_usbd_cdc_acm_bytes_stored(&m_app_cdc_acm) == 1)
+            {
+                interval = 1800; //30 mins in sec
+            }
+            if (app_usbd_cdc_acm_bytes_stored(&m_app_cdc_acm) == 2)
+            {
+                interval = 3600; //1 hr in sec
+            }
+            if (app_usbd_cdc_acm_bytes_stored(&m_app_cdc_acm) == 3)
+            {
+                interval = 7200; //2 hr in sec
+            }
+            if (app_usbd_cdc_acm_bytes_stored(&m_app_cdc_acm) == 4)
+            {
+                interval = 21600; //6 hr in sec
+            }
+            if (app_usbd_cdc_acm_bytes_stored(&m_app_cdc_acm) == 5)
+            {
+                interval = 43200; //12 hr in sec
+            }
+            if (app_usbd_cdc_acm_bytes_stored(&m_app_cdc_acm) == 6)
+            {
+                interval = 86400; //1 day in sec
+            }
+            if (app_usbd_cdc_acm_bytes_stored(&m_app_cdc_acm) == 7)
+            {
+                interval = 604800; //1 week in sec
+            }
+            if (app_usbd_cdc_acm_bytes_stored(&m_app_cdc_acm) == 8)
+            {
+                interval = 2416200; //4 week in sec
+            }
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+static void usbd_user_ev_handler(app_usbd_event_type_t event)
+{
+    switch (event)
+    {
+        case APP_USBD_EVT_DRV_SUSPEND:
+            //bsp_board_led_off(LED_USB_RESUME);
+            break;
+        case APP_USBD_EVT_DRV_RESUME:
+            //bsp_board_led_on(LED_USB_RESUME);
+            break;
+        case APP_USBD_EVT_STARTED:
+            break;
+        case APP_USBD_EVT_STOPPED:
+            app_usbd_disable();
+            bsp_board_leds_off();
+            break;
+        case APP_USBD_EVT_POWER_DETECTED:
+            //NRF_LOG_INFO("USB power detected");
+
+            if (!nrf_drv_usbd_is_enabled())
+            {
+                app_usbd_enable();
+            }
+            break;
+        case APP_USBD_EVT_POWER_REMOVED:
+            //NRF_LOG_INFO("USB power removed");
+            app_usbd_stop();
+            break;
+        case APP_USBD_EVT_POWER_READY:
+            //NRF_LOG_INFO("USB ready");
+            app_usbd_start();
+            break;
+        default:
+            break;
+    }
+}
 
 /**@brief Names which the central applications will scan for, and which will be advertised by the peripherals.
  *  if these are set to empty strings, the UUIDs defined below will be used
@@ -1166,9 +1321,74 @@ int main(void)
 
     scan_init();
     // Start execution.
-    NRF_LOG_INFO("bitch ");
     scanning_start(&erase_bonds);
 
+    int Temp1 = 15;
+    int Temp2 = 17;
+    int Volt1 = 21;
+    int Volt2 = 23;
+
+    //Keep not sure exactly but breaks without
+    ret_code_t ret;
+    static const app_usbd_config_t usbd_config = {
+        .ev_state_proc = usbd_user_ev_handler
+    };
+
+    ret = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(ret);
+
+    ret = nrf_drv_clock_init();
+    APP_ERROR_CHECK(ret);
+    
+    nrf_drv_clock_lfclk_request(NULL);
+
+    //Check if available
+    while(!nrf_drv_clock_lfclk_is_running())
+    {
+         //Just waiting 
+    }
+
+    //get port
+    app_usbd_serial_num_generate();
+
+    ret = app_usbd_init(&usbd_config);
+    APP_ERROR_CHECK(ret);
+
+    app_usbd_class_inst_t const * class_cdc_acm = app_usbd_cdc_acm_class_inst_get(&m_app_cdc_acm);
+    ret = app_usbd_class_append(class_cdc_acm);
+    APP_ERROR_CHECK(ret);
+
+    if (USBD_POWER_DETECTION)
+    {
+        ret = app_usbd_power_events_enable();
+        APP_ERROR_CHECK(ret);
+    }
+    else
+    {
+        app_usbd_enable();
+        app_usbd_start();
+    }
+
+    while(true)
+    {
+        while (app_usbd_event_queue_process())
+        {
+             //Nothing to do 
+        }
+        //change flag when want to send
+        if(m_send_flag)
+        {
+            bsp_board_led_invert(0);
+
+            size_t size = sprintf(m_tx_buffer, "%u.%u,%u.%u\r\n", Temp1,Temp2,Volt1,Volt2);
+            ret = app_usbd_cdc_acm_write(&m_app_cdc_acm, m_tx_buffer, size);
+
+        }  
+
+        UNUSED_RETURN_VALUE(NRF_LOG_PROCESS());
+         //Sleep CPU only if there was no interrupt since last loop processing 
+        __WFE();
+    }
     // Enter main loop.
     for (;;)
     {
